b81618b47147afb756d0d9bded10fab18b47b750
laurinehrd <laurine.hrd@hotmail.fr>
2020-10-11 14:30:53 +0200

    add php config, make pin entity timestampable, listing on index by createdAt desc

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 use App\Repository\PinRepository;
 use App\Entity\Pin;
@@ -14,9 +15,9 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository): Response
     {
-        $pins = $pinRepository->findAll();
+        $pins = $pinRepository->findBy([], ['createdAt' => 'DESC']);
 
         return $this->render('pins/index.html.twig', compact('pins'));
     }
@@ -24,7 +25,7 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
      */
-    public function show(Pin $pin)
+    public function show(Pin $pin): Response
     {
         return $this->render('pins/show.html.twig', compact('pin'));
     }
